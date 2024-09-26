bd641b29f58d13f874309ee4f8e8496c573d31c5
Ziothh <louisgiet.w@gmail.com>
2024-09-26 15:33:40 +0200

    changed config from mjs to ts as per the documentation

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from 'next'
+ 
+// Code copied from https://nextjs.org/docs/pages/building-your-application/configuring/typescript#type-checking-nextconfigts
+const nextConfig: NextConfig = {
+  /* config options here */
+}
+ 
+export default nextConfig
